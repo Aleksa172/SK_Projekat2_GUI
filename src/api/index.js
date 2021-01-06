b75897111c4b2e986b5123c2f1e133284fc2785e
Aleksa Smiljanic <asmiljanic16@raf.rs>
2021-01-06 14:03:00 +0100

    Inicijalni komit

---
+++ src/api/index.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+axios.interceptors.request.use(config => {
+    handleRequestInterceptor(config);
+    return config;
+});
+
+function handleRequestInterceptor(config) {
+    try {
+        let jwt = sessionStorage.getItem("jwt");
+        console.log('jwt', jwt);
+    }
+    catch(e) {
+        console.error("INTERCEPTOR ERROR: "+e);
+    }
+}
+
+
+
+var SERVIS1_URL = "http://localhost:8081";
+var SERVIS2_URL = "http://localhost:8082";
+var SERVIS3_URL = "http://localhost:8083";
+
+export default {
+
+    login: (params) => axios({
+      url: `${SERVIS1_URL}/login`,
+      method: 'POST',  
+      data: {
+          email: params.email,
+          password: params.password
+      }
+    });
+
+}
